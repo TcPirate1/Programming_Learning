80c6ecd0e4e74fd4236d7818266e44d298bf4bb4
TC <88120195+TcPirate1@users.noreply.github.com>
2022-04-25 15:46:49 +1200

    Update BoxCalculator.py
    imported math library so that math.floor can be used to floor Big_Box and Medium_Box

--- BoxCalculator.py
+++ BoxCalculator.py
@@ -1,3 +1,4 @@
+import math
 #Setting up variables for the different sizes of boxes
 Big = 5
 Medium = 3
@@ -7,10 +8,10 @@ Small = 1
 items = int(input('How many items do you have? ')) #This is the input variable for the user to input their value. Casted to integer so program only runs if an integer is inputted.
 
 #Below is the code to calculate the amount of boxes needed to store the items
-Big_Box = items / Big
+Big_Box = math.floor(items / Big)
 Big_Box_Remainder = items % Big
 
-Medium_Box = Big_Box_Remainder / Medium
+Medium_Box = math.floor(Big_Box_Remainder / Medium)
 
 Small_Box = Big_Box_Remainder % Medium
 
@@ -20,3 +21,5 @@ print(f'You will need {int(Big_Box+Medium_Box+Small_Box)} Boxes in total.\n\nYou
 #Test assertion1: input of 5 items should return 1 box with only 1 big box.
 #Test assertion2: input of 51 items should return 11 boxes with 10 big boxes and 1 small box.
 #Test assertion3: input of 17 items should return 5 boxes with 3 big boxes and 2 small boxes.
+
+#Medium_Box and Big_Box results in decimals which causes rounding issues when added together.
