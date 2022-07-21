3d31725242f177b2d83bd7e665325da73765f47c
TC <88120195+TcPirate1@users.noreply.github.com>
2022-07-21 14:05:09 +1200

    Add files via upload

---
+++ Lottery/LottoSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    internal class LottoSystem : IObserver<Player>
+    {
+        private readonly Random random = new();
+        private IDisposable cancellation;
+        public virtual void Subscribe(LottoDrawer provider)
+        {
+            cancellation = provider.Subscribe(this);
+        }
+        public virtual void Unsubscribe()
+        {
+            cancellation.Dispose();
+        }
+        public virtual void OnCompleted()
+        {
+        }
+        public virtual void OnError(Exception e)
+        {
+            //No implementation
+        }
+
+        public virtual void OnNext(Player info)
+        {
+            int count = 0;
+            int balance = info.PlayerBudget;
+
+            if (info != null)
+            {
+                count++;
+                Console.WriteLine("Welcome to a new week of Lotto.");
+                for (int i = 0; info.PlayerName.Length > 0; i++)
+                {
+                    Console.WriteLine(info.PlayerName[i]);
+                }
+                balance =- info.Numbers.Length * count;
+                int Result = random.Next(1, 50);
+                Console.WriteLine($"This week's winning number is:...{Result}!");
+            }
+        }
+    }
+}
---
+++ Lottery/Unsubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    internal class Unsubscriber<Player> : IDisposable
+    {
+        private readonly List<IObserver<Player>> _observers;
+        private readonly IObserver<Player> _observer;
+        internal Unsubscriber(List<IObserver<Player>> observers, IObserver<Player> observer)
+        {
+            this._observers = observers;
+            this._observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+}
